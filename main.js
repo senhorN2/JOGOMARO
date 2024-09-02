98bbb3fef7f40701680ed6df49f7bc87cf211f78
xx6MarcianoEstudador9xx <164871749+senhorN2@users.noreply.github.com>
2024-09-02 07:53:09 -0300

    fsfds

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-const mario = document.querySelector('.mario');
-const pipe = document.querySelector('.pipe');
+const mario = document.querySelector(".mario");
+const pipe = document.querySelector(".pipe");
 const jump = () => {
-    mario.classList.add('jump');
+    mario.classList.add("jump");
     setTimeout(() => {
-        mario.classList.remove('jump');
+        mario.classList.remove("jump");
     }
     ,500);
 }
-const loop = setInterval (() => {
+const loop = setInterval (() => 
 const pipePosition = pipe.offsetLeft;
 const marioPosition = +window.getComputedStyle(mario).bottom.replace("px","");
 if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
