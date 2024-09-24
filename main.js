53960d55e869ff736062e4591cd07889187049a0
xx6MarcianoEstudador9xx <164871749+senhorN2@users.noreply.github.com>
2024-09-24 08:47:05 -0300

    gfgf

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ const jump = () => {
 }
 
 const updateScore = () => {
-    score += 1;
+    score += 10;
     scoreElement.textContent = score;
     const animationSpeed = 1.5/ (1 + score/ 500);
     pipe.style.animation = `pipe-animation ${animationSpeed}s infinite linear`;
